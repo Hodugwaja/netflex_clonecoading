2cd59ad0cdce116b3ed3246a40692954fae6a856
HoduGwaja <honeybutter9250@gmail.com>
2021-05-15 11:04:40 +0900

    [Netflex | 2021.05.15 | changed starter project]

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_netflix_responsive_ui/screens/screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Netflix UI',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.black,
       ),
-      home: NavScreen(),
+      home: Scaffold(),
     );
   }
 }
